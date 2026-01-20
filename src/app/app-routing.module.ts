d7d561215f519d7d63e54fc9ffa1bc0f2a9423e2
unknown <shivendra.kadam@conexus-ns.com>
2026-01-20 17:30:24 +0530

    browser list changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+   { path: 'quran', loadChildren: () => import('./quran/quran.module').then(m => m.QuranModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'quran', loadChildren: () => import('./quran/quran.module').then(m => m.QuranModule) },
+ 
   { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
   { path: '', redirectTo: 'quran', pathMatch: 'full' }, // Default route
-  
+  { path: '**', redirectTo: 'quran' } // Wildcard route
 ];
 
 @NgModule({

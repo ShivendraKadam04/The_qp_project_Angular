42130d896590ef077b18bddf2b89bf650e32b876
unknown <shivendra.kadam@conexus-ns.com>
2026-01-20 12:53:28 +0530

    browser list changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
+// 🔴 ADD THIS
+window.onerror = function (message, source, lineno, colno, error) {
+  console.log('🔥 GLOBAL ERROR');
+  console.log({ message, source, lineno, colno, error });
+};
+
+window.onunhandledrejection = function (event) {
+  console.log('🔥 UNHANDLED PROMISE');
+  console.log(event.reason);
+};
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
   .catch(err => console.error(err));

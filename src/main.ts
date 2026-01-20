32ae364830571ad80b3dcd19b88eda1b9d7400b0
unknown <shivendra.kadam@conexus-ns.com>
2026-01-20 13:59:42 +0530

    browser list changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-// 🔴 ADD THIS
-console.log('✅ BOOT: main.ts loaded');
-
-window.addEventListener('error', (e: any) => {
-  console.log('🔥 WINDOW ERROR:', e?.message, e?.filename, e?.lineno, e?.colno, e?.error);
-}, true);
-
-window.addEventListener('unhandledrejection', (e: any) => {
-  console.log('🔥 UNHANDLED REJECTION:', e?.reason);
-});
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true,
+})
+  .catch(err => console.error(err));

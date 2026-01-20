a00207ca560eff217417943299e658bf788bae62
unknown <shivendra.kadam@conexus-ns.com>
2026-01-20 17:16:40 +0530

    browser list changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'quran', loadChildren: () => import('./quran/quran.module').then(m => m.QuranModule) },
   { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
-  { path: '', redirectTo: '/auth', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: 'quran', pathMatch: 'full' }, // Default route
   
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,34 +1,15 @@
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthComponent } from './auth/auth.component';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
-
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzCollapseModule } from 'ng-zorro-antd/collapse';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
 
 @NgModule({
   declarations: [
@@ -42,27 +23,7 @@ import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
     AuthRoutingModule,
     NzDividerModule,
     ReactiveFormsModule,
-    NzButtonModule,
-     CommonModule,
-        FormsModule, 
-        NzButtonModule,
-        NzEmptyModule,
-        NzInputModule,
-        NzModalModule,
-        NzCheckboxModule,
-        NzBreadCrumbModule,
-        NzIconModule,
-        NzMenuModule,
-        NzDropDownModule,
-        NzLayoutModule,
-        NzSpinModule,
-        NzPopoverModule,
-        NzTabsModule,
-        NzFormModule,
-        NzSelectModule,
-        NzAutocompleteModule, // Added for autocomplete
-        ReactiveFormsModule,
-        NzCollapseModule
+    NzButtonModule
   ]
 })
 export class AuthModule { }

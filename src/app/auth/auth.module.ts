eb467e69c5400e2c7a5a3f98c552b56508821610
unknown <shivendra.kadam@conexus-ns.com>
2026-01-20 17:04:44 +0530

    browser list changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthComponent } from './auth/auth.component';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 
 @NgModule({
   declarations: [
@@ -23,7 +42,27 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     AuthRoutingModule,
     NzDividerModule,
     ReactiveFormsModule,
-    NzButtonModule
+    NzButtonModule,
+     CommonModule,
+        FormsModule, 
+        NzButtonModule,
+        NzEmptyModule,
+        NzInputModule,
+        NzModalModule,
+        NzCheckboxModule,
+        NzBreadCrumbModule,
+        NzIconModule,
+        NzMenuModule,
+        NzDropDownModule,
+        NzLayoutModule,
+        NzSpinModule,
+        NzPopoverModule,
+        NzTabsModule,
+        NzFormModule,
+        NzSelectModule,
+        NzAutocompleteModule, // Added for autocomplete
+        ReactiveFormsModule,
+        NzCollapseModule
   ]
 })
 export class AuthModule { }
